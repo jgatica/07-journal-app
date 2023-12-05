754ef59ef17a8403da8cbd46b164ab17d55e9d0f
Jorge Gatica <jorge.gatica@gmail.com>
2023-12-05 03:31:41 -0300

    Componente NoEntrySelected: Se crea Fab(boton mas) y se importa en NoEntrySelected

---
+++ src/modules/daybook/components/Fab.vue
@@ -0,0 +1,15 @@
+<template>
+  <button>
+    <i class="fa fa-2x fa-plus"></i>
+  </button>
+</template>
+
+<script>
+export default {
+  name: "Fab"
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
--- src/modules/daybook/views/NoEntrySelected.vue
+++ src/modules/daybook/views/NoEntrySelected.vue
@@ -4,11 +4,17 @@
       No hay nada seleccionado
     </h1>
   </div>
+  <Fab></Fab>
 </template>
 
 <script>
+import {defineAsyncComponent} from "vue";
+
 export default {
-  name: "NoEntrySelected"
+  name: "NoEntrySelected",
+  components: {
+    Fab: defineAsyncComponent(()=> import(/* webpackChunkName: "Fab" */'@/modules/daybook/components/Fab.vue')),
+  }
 }
 </script>
 
