f1942c1139d30ac786b1fd3e5fe1f1024fadb4eb
Jorge Gatica <jorge.gatica@gmail.com>
2023-12-05 02:55:44 -0300

    Componente NoEntrySelected: Creo NoEntrySelected.vue para cuando no haya entrada seleccionada

---
+++ src/modules/daybook/views/NoEntrySelected.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="d-flex justify-content-center">
+    <h1 class="align-self-center">
+      No hay nada seleccionado
+    </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NoEntrySelected"
+}
+</script>
+
+<style scoped lang="scss">
+  div {
+    height: 100%;
+  }
+</style>
