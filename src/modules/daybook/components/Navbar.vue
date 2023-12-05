18ecef8237d5556575fed1ba3a6d3da27b830eb5
Jorge Gatica <jorge.gatica@gmail.com>
2023-12-05 00:30:23 -0300

    Estructura del DaybookLayout: Creo Navbar y se importa en layout

---
+++ src/modules/daybook/components/Navbar.vue
@@ -0,0 +1,11 @@
+<template>
+  <nav class="navbar bg-primary">
+    jagl
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Navbar"
+}
+</script>
--- src/modules/daybook/layouts/DayBookLayout.vue
+++ src/modules/daybook/layouts/DayBookLayout.vue
@@ -1,10 +1,16 @@
 <template>
   <h1>Daybook Layout</h1>
+  <Navbar />
 </template>
 
 <script>
+import {defineAsyncComponent} from "vue";
+
 export default {
-  name: "DayBookLayouts"
+  name: "DayBookLayouts",
+  components: {
+    Navbar : defineAsyncComponent(() => import(/* webpackChunkName: "Navbar" */ '@/modules/daybook/components/Navbar.vue'))
+  },
 }
 </script>
 
