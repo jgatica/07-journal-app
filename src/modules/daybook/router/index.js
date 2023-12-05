90fce836de43c05dd98a7321a0bc967befb10919
Jorge Gatica <jorge.gatica@gmail.com>
2023-12-04 23:49:03 -0300

    Estructura modular: Bases para enrutar al modulo

---
+++ src/modules/daybook/router/index.js
@@ -0,0 +1,7 @@
+export default {
+  name: 'daybook',
+  component: () => import(/* webpackChunkName: "DayBookLayout" */ '@/modules/daybook/layouts/DayBookLayout.vue'),
+  children: [
+
+  ],
+}
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,6 +6,17 @@
     <button class="btn btn-primary">Primary</button>
     <button class="btn btn-secondary">Secondary</button>
     <button class="btn btn-success">Success</button>
+    <button @click="goToDaybook" class="btn btn-success">Ir a la ruta daybook</button>
 
   </div>
 </template>
+
+<script>
+export default {
+  methods: {
+    goToDaybook() {
+      this.$router.push({ name: 'daybook' })
+    },
+  }
+}
+</script>
