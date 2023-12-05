1b916d07cea25a45b4fb5d2d6036efcdead3ff8e
Jorge Gatica <jorge.gatica@gmail.com>
2023-12-05 02:21:46 -0300

    Componentes EntryList y Entry: se agrega input text y agrega un v-for

--- src/modules/daybook/components/EntryList.vue
+++ src/modules/daybook/components/EntryList.vue
@@ -1,5 +1,18 @@
 <template>
-  <h1>EntryList!</h1>
+  <div class="entry-list-container">
+    <div class="px-2 pt-2">
+      <input type="text"
+             class="form-control"
+             placeholder="Buscar entrada"
+      />
+    </div>
+    <div class="entry-scrollarea">
+      <h2
+        v-for="item in 100"
+        :key="item"
+      >Entry</h2>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -8,6 +21,13 @@ export default {
 }
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
+.entry-list-container {
+  border-right: 1px solid #2c3e50;
+  height: calc(100vh - 56px);
+}
+.entry-scrollarea {
+  height: calc(100vh - 110px);
+  overflow: scroll;
+}
 </style>
