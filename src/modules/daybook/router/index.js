50390098f26fd51a71fc670e40e2455ebf0018e1
Jorge Gatica <jorge.gatica@gmail.com>
2023-12-05 04:58:09 -0300

    EntryView: Se crea EntryView.vue y se agrega ruta para acceder a el

--- src/modules/daybook/router/index.js
+++ src/modules/daybook/router/index.js
@@ -7,6 +7,11 @@ export default {
       name: 'no-entry',
       component: ()=> import(/* webpackChunkName: "NoEntrySelected" */'@/modules/daybook/views/NoEntrySelected.vue'),
     },
+    {
+      path: ':id',
+      name: 'entry',
+      component: ()=> import(/* webpackChunkName: "EntryView" */'@/modules/daybook/views/EntryView.vue'),
+    },
   ],
 }
 
---
+++ src/modules/daybook/views/EntryView.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>Hola mundo</h1>
+</template>
+
+<script>
+export default {
+  name: "EntryView"
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
