1a781e43dc2f4a2720b5cb0a3e9b715bf0310d85
Jorge Gatica <jorge.gatica@gmail.com>
2023-12-05 04:48:55 -0300

    Componente NoEntrySelected: Se da estilos al boton

--- src/modules/daybook/components/Fab.vue
+++ src/modules/daybook/components/Fab.vue
@@ -1,5 +1,5 @@
 <template>
-  <button>
+  <button class="btn btn-primary">
     <i class="fa fa-2x fa-plus"></i>
   </button>
 </template>
@@ -11,5 +11,12 @@ export default {
 </script>
 
 <style scoped lang="scss">
-
+button {
+  position: fixed;
+  bottom: 20px;
+  right: 20px;
+  width: 80px;
+  height: 80px;
+  border-radius: 100%;
+}
 </style>
