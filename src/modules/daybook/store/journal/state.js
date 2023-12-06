fea59c38c532bfe0bb3dfad4a42c0af2c029a60f
Jorge Gatica <jorge.gatica@gmail.com>
2023-12-06 03:29:42 -0300

    Entradas ficticias y punto de restauración: Se crean entradas de ejemplo

--- src/modules/daybook/store/journal/state.js
+++ src/modules/daybook/store/journal/state.js
@@ -1,4 +1,23 @@
 export default () => ({
   isLoading: true,
-  entries: [1,2,3,4,5,6]
+  entries: [
+    {
+      id: new Date().getTime(),
+      date: new Date().toDateString(),
+      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation',
+      picture: null,
+    },
+    {
+      id: new Date().getTime() + 1000,
+      date: new Date().toDateString(),
+      text: 'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation',
+      picture: null,
+    },
+    {
+      id: new Date().getTime() + 2000,
+      date: new Date().toDateString(),
+      text: 'Ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation',
+      picture: null,
+    },
+  ],
 })
