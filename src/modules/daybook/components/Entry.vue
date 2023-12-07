d55f48e1dac9cc392d875c10d7b78b78fdcc20e4
Jorge Gatica <jorge.gatica@gmail.com>
2023-12-06 23:10:47 -0300

    EntryComponent - Información al componente: Formatear fecha

--- src/modules/daybook/components/Entry.vue
+++ src/modules/daybook/components/Entry.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="entry-container mb-3 pointer p-2"
-       @click="$router.push({ name:'entry', params: { id:10 } })"
+       @click="$router.push({ name:'entry', params: { id:entry.id } })"
   >
     <div class="entry-title d-flex">
-      <span class="text-success fs-5 fw-bold">{{ entry.id }}</span>
-      <span class="mx-1 fs-5">{{ entry.date }}</span>
-      <span class="mx-2 fw-light">{{ entry.picture }}</span>
+      <span class="text-success fs-5 fw-bold">{{ day }}</span>
+      <span class="mx-1 fs-5">{{ month }}</span>
+      <span class="mx-2 fw-light">{{ yearDay }}</span>
     </div>
 
     <div class="entry-description">
@@ -16,6 +16,10 @@
 </template>
 
 <script>
+
+const months = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio','Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre']
+const days   = ['Domingo','Lunes','Martes','Miércoles','Jueves','Viernes','Sábado']
+
 export default {
   name: "Entry",
   props: {
@@ -29,6 +33,18 @@ export default {
       return (this.entry.text.length > 130)
         ? this.entry.text.substring(0, 130) + '...'
         : this.entry.text
+    },
+    day() {
+      const date = new Date(this.entry.date)
+      return date.getDate()
+    },
+    month() {
+      const date = new Date(this.entry.date)
+      return months[date.getMonth()]
+    },
+    yearDay() {
+      const date = new Date(this.entry.date)
+      return `${date.getFullYear()}, ${days[date.getDay()]}`
     },
   },
 }
