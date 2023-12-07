c1c0feae03ac0e3c47b16090a7eba5e066f5347e
Jorge Gatica <jorge.gatica@gmail.com>
2023-12-06 23:22:31 -0300

    Refactoriza funcion getEntriesByTerm

--- src/modules/daybook/store/journal/getters.js
+++ src/modules/daybook/store/journal/getters.js
@@ -3,11 +3,7 @@
 // }
 
 export const getEntriesByTerm = (state) => (term = '') => {
-  if (term.length === 0) {
-    console.log(state.entries)
-
-    return state.entries
-  }
+  if (term.length === 0) return state.entries
 
   return state.entries.filter(entry => entry.text.toLowerCase().includes(term.toLowerCase()))
 }
