0ee0d898ec75b4ab5db1f1e74f2211337d17de50
Jorge Gatica <jorge.gatica@gmail.com>
2023-12-07 23:50:29 -0300

    Realizar el commit de una mutación: Cambio estructura

--- src/modules/daybook/store/journal/actions.js
+++ src/modules/daybook/store/journal/actions.js
@@ -2,11 +2,15 @@ import journalApi from "@/api/journalApi";
 
 export const loadEntries = async () => {
   const {data} = await journalApi.get('/entries.json')
-
+  const entries = []
   for (let id of Object.keys(data)) {
-    console.log(id )
+    entries.push({
+      id,
+      ...data[id]
+    })
   }
 
+  console.log(entries)
 }
 
 export const updateEntries = async () => {
