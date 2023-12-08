46e6a7e62d6192ea4da184277d0eecdba838d042
Jorge Gatica <jorge.gatica@gmail.com>
2023-12-07 23:04:09 -0300

    Realizar el commit de una mutación: Obtener IDS

--- src/modules/daybook/store/journal/actions.js
+++ src/modules/daybook/store/journal/actions.js
@@ -2,7 +2,11 @@ import journalApi from "@/api/journalApi";
 
 export const loadEntries = async () => {
   const {data} = await journalApi.get('/entries.json')
-  console.log(data)
+
+  for (let id of Object.keys(data)) {
+    console.log(id )
+  }
+
 }
 
 export const updateEntries = async () => {
