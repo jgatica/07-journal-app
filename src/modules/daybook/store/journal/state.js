83c0ef3b200746bc8d004f6604acd18956f1a487
Jorge Gatica <jorge.gatica@gmail.com>
2023-12-08 04:13:13 -0300

    Realizar el commit de una mutación: Se borran la entries de state.

--- src/modules/daybook/store/journal/state.js
+++ src/modules/daybook/store/journal/state.js
@@ -1,23 +1,4 @@
 export default () => ({
   isLoading: true,
-  entries: [
-    {
-      id: '1',
-      date: new Date().toDateString(),
-      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation',
-      picture: null,
-    },
-    {
-      id: '2',
-      date: new Date().toDateString(),
-      text: 'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation',
-      picture: null,
-    },
-    {
-      id: '3',
-      date: new Date().toDateString(),
-      text: 'Ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation',
-      picture: null,
-    },
-  ],
+  entries: [],
 })
