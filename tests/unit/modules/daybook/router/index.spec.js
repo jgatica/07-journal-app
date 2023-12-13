db568d436858c8289fc6b3512047203e85e18f5b
Jorge Gatica <jorge.gatica@gmail.com>
2023-12-13 20:12:45 -0300

    Daybook Router: Reemplazo las funciones por expect.any(Function) ahora pasa el test

--- tests/unit/modules/daybook/router/index.spec.js
+++ tests/unit/modules/daybook/router/index.spec.js
@@ -4,26 +4,22 @@ describe('Pruebas en el router module del Daybook', () => {
 
   it('el router debe de tener esta configuración', () => {
     expect(daybookRouter).toMatchObject({
-        name: 'daybook',
-        component: () => import(/* webpackChunkName: "DayBookLayout" */ '@/modules/daybook/layouts/DayBookLayout.vue'),
-        children: [
-          {
-            path: '',
-            name: 'no-entry',
-            component: ()=> import(/* webpackChunkName: "NoEntrySelected" */'@/modules/daybook/views/NoEntrySelected.vue'),
-          },
-          {
-            path: ':id',
-            name: 'entry',
-            component: ()=> import(/* webpackChunkName: "EntryView" */'@/modules/daybook/views/EntryView.vue'),
-            props: (route) => {
-              return {
-                id: route.params.id
-              }
-            }
-          },
-        ],
-      }) // Fin expect
+      name: 'daybook',
+      component: expect.any(Function),
+      children: [
+        {
+          path: '',
+          name: 'no-entry',
+          component: expect.any(Function),
+        },
+        {
+          path: ':id',
+          name: 'entry',
+          component: expect.any(Function),
+          props: expect.any(Function)
+        },
+      ],
+    }) // Fin expect
 
   });
 
