1446d07fb660a8735f32136ef05b2631d5ecdb25
Jorge Gatica <jorge.gatica@gmail.com>
2023-12-13 18:17:50 -0300

    Tarea - Pruebas en el FAB Component: debe mostrar el icono por defecto

--- tests/unit/modules/daybook/components/fab.spec.js
+++ tests/unit/modules/daybook/components/fab.spec.js
@@ -3,4 +3,11 @@ import Fab from "@/modules/daybook/components/Fab.vue";
 
 describe('Pruebas a Fab Componente', () => {
 
+  it('debe mostrar el icono por defecto ', () => {
+    const wrapper = shallowMount(Fab)
+    const iTag = wrapper.find('i')
+
+    expect(iTag.classes('fa-plus')).toBe(true)
+  })
+
 });
