2cd6e0cf4366e3fb8ca72c3365d7a4fc3e1a4f46
Jorge Gatica <jorge.gatica@gmail.com>
2023-12-13 19:39:06 -0300

    Daybook Router: Se crea archivo de test para las rutas

---
+++ tests/unit/modules/daybook/router/index.spec.js
@@ -0,0 +1,9 @@
+import daybookRouter from "@/modules/daybook/router";
+
+describe('Pruebas en el router module del Daybook', () => {
+
+  it('el router debe de tener esta configuración', () => {
+
+  });
+  
+});
