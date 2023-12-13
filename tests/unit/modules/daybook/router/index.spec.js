5d43d8768848062966b5cb0d93a926bc3a4907d6
Jorge Gatica <jorge.gatica@gmail.com>
2023-12-13 19:50:15 -0300

    Daybook Router: PREPARANDO TEST 'el router debe de tener esta configuración'

--- tests/unit/modules/daybook/router/index.spec.js
+++ tests/unit/modules/daybook/router/index.spec.js
@@ -3,7 +3,28 @@ import daybookRouter from "@/modules/daybook/router";
 describe('Pruebas en el router module del Daybook', () => {
 
   it('el router debe de tener esta configuración', () => {
+    expect(daybookRouter).toMatchObject({
+        name: 'daybook',
+        component: () => import(/* webpackChunkName: "DayBookLayout" */ '@/modules/daybook/layouts/DayBookLayout.vue'),
+        children: [
+          {
+            path: '',
+            name: 'no-entry',
+            component: ()=> import(/* webpackChunkName: "NoEntrySelected" */'@/modules/daybook/views/NoEntrySelected.vue'),
+          },
+          {
+            path: ':id',
+            name: 'entry',
+            component: ()=> import(/* webpackChunkName: "EntryView" */'@/modules/daybook/views/EntryView.vue'),
+            props: (route) => {
+              return {
+                id: route.params.id
+              }
+            }
+          },
+        ],
+      }) // Fin expect
 
   });
-  
+
 });
