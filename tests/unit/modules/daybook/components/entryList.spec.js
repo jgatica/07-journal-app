4da303b690408150d6bdb10302aa3eba25056a6a
Jorge Gatica <jorge.gatica@gmail.com>
2023-12-22 06:47:49 -0300

    Pruebas en el componente EntryList: En el state, se ocupa las entries que ya tengo en las pruebas

--- tests/unit/modules/daybook/components/entryList.spec.js
+++ tests/unit/modules/daybook/components/entryList.spec.js
@@ -4,6 +4,7 @@ import {createStore} from "vuex";
 import EntryList from "@/modules/daybook/components/EntryList.vue";
 import journal from "@/modules/daybook/store/journal";
 import {getEntriesByTerm} from "@/modules/daybook/store/journal/getters";
+import testJournalState from "../../../mock-data/test-journal-state";
 
 describe('Pruebas al componente EntryList', () => {
 
@@ -11,7 +12,11 @@ describe('Pruebas al componente EntryList', () => {
   const journalMockModule = {
     namespaced: true,
     getters: {
-      getEntriesByTerm: getEntriesByTerm
+      getEntriesByTerm: getEntriesByTerm,
+      state: ()=> ({
+        isLoading: false,
+        entries: testJournalState.entries,
+      })
     }
   }
 
