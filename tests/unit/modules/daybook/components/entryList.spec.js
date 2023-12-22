be415dd4cabef3cb4568fabe5b543ced4d9875e5
Jorge Gatica <jorge.gatica@gmail.com>
2023-12-22 08:05:20 -0300

    EntryList - Segunda parte de pruebas: Se elimina la creacion del store independiente

--- tests/unit/modules/daybook/components/entryList.spec.js
+++ tests/unit/modules/daybook/components/entryList.spec.js
@@ -2,29 +2,8 @@ import {shallowMount} from "@vue/test-utils";
 import {createStore} from "vuex";
 
 import EntryList from "@/modules/daybook/components/EntryList.vue";
-import journal from "@/modules/daybook/store/journal";
-import {getEntriesByTerm} from "@/modules/daybook/store/journal/getters";
-import testJournalState from "../../../mock-data/test-journal-state";
 
 describe('Pruebas al componente EntryList', () => {
-
-  // Crear un mock completo de ese modulo
-  const journalMockModule = {
-    namespaced: true,
-    getters: {
-      getEntriesByTerm: getEntriesByTerm,
-    },
-    state: ()=> ({
-      isLoading: false,
-      entries: testJournalState.entries,
-    })
-  }
-
-  const store = createStore({
-    modules: {
-      journal: {...journalMockModule},
-    }
-  })
 
   const wrapper = shallowMount(EntryList, {
     global: {
