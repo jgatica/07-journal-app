711455cc7cf3eb42b6248adad963adf3a41a8764
Jorge Gatica <jorge.gatica@gmail.com>
2023-12-22 08:12:42 -0300

    EntryList - Segunda parte de pruebas: Se crea store con el store real de la app

--- tests/unit/modules/daybook/components/entryList.spec.js
+++ tests/unit/modules/daybook/components/entryList.spec.js
@@ -2,9 +2,21 @@ import {shallowMount} from "@vue/test-utils";
 import {createStore} from "vuex";
 
 import EntryList from "@/modules/daybook/components/EntryList.vue";
+import journal from "@/modules/daybook/store/journal";
+import journalState from "../../../mock-data/test-journal-state";
+
+const createVuexStore = (initialState) => createStore({
+  modules: {
+    journal: {
+      ...journal,
+      state: {...initialState} // 👈 Sobrescribe al state que esta desectucturado arriba
+    },
+  }
+}) // Fin createVuexStore
 
 describe('Pruebas al componente EntryList', () => {
 
+  const store = createVuexStore(journalState)
   const wrapper = shallowMount(EntryList, {
     global: {
       mocks: {
