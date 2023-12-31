32e09e9e44831e9a95cf7742858f3a66a01e6868
Jorge Gatica <jorge.gatica@gmail.com>
2023-12-31 20:07:48 -0300

    Actualizar el displayName del usuario: Actualizar el nombre de usuario

--- src/modules/auth/store/actions.js
+++ src/modules/auth/store/actions.js
@@ -5,7 +5,9 @@ export const createUser = async ({ commit }, user) => {
 
   try {
     const { data } = await authApi.post(':signUp', { email, password, returnSecureToken: true })
-    console.log(data)
+    const { idToken, refreshToken } = data
+    const resp = await authApi.post(':update', { displayName: name, idToken: idToken })
+    console.log(resp)
 
     return { ok: true }
   } catch (error) {
