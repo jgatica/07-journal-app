784c46e43a394add89726d5e9351e3b5ef8a01da
Jorge Gatica <jorge.gatica@gmail.com>
2023-12-31 01:20:36 -0300

    Rutas de Login y Register: Se agrega ruta hija que mostrara formulario login

--- src/modules/auth/router/index.js
+++ src/modules/auth/router/index.js
@@ -2,6 +2,10 @@ export default {
   name: 'auth',
   component: ()=>import(/* webpackChunkName: "auth" */'@/modules/auth/layouts/AuthLayout.vue'),
   children: [
-
+    {
+      path: '',
+      name: 'login',
+      component: () => import(/* webpackChunkName: "login" */'@/modules/auth/views/Login.vue'),
+    },
   ],
 }
