f0fd7361f8a4792ce15b58f7824d6afed845c14c
Jorge Gatica <jorge.gatica@gmail.com>
2024-01-03 06:24:50 -0300

    AuthGuard: Restringir el acceso a ruta

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,8 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import daybookRouter from "@/modules/daybook/router";
+import daybookRouter from '@/modules/daybook/router'
 import authRouter from '@/modules/auth/router'
+import isAuthenticatedGuard from '@/modules/auth/router/auth-guard'
 
 const routes = [
   {
@@ -23,6 +24,7 @@ const routes = [
   },
   {
     path: '/daybook',
+    beforeEnter: [isAuthenticatedGuard],
     ...daybookRouter,
   },
 ]
