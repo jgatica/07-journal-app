621f3b4a4cd831b64f9bc3643cb1694f637cb925
Jorge Gatica <jorge.gatica@gmail.com>
2024-01-04 05:19:34 -0300

    Interceptores: Autenticar usuario en cada peticion http

--- src/api/journalApi.js
+++ src/api/journalApi.js
@@ -4,4 +4,17 @@ const journalApi = axios.create({
   baseURL: 'https://vue-demos-eff42-default-rtdb.firebaseio.com'
 })
 
+journalApi.interceptors.request.use((config) => {
+  config.params = {
+    auth: localStorage.getItem('idToken')
+  }
+
+  // config.headers = {
+  //   Authorization: 'beared idToken'
+  // }
+
+  return config
+
+})
+
 export default journalApi
