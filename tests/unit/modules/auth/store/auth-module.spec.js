cfeefebbc0e9dd4b14797865111c12b4221d5054
Jorge Gatica <jorge.gatica@gmail.com>
2024-01-05 07:35:22 -0300

    Mutations - loginUser: Test Mutation: loginUser

--- tests/unit/modules/auth/store/auth-module.spec.js
+++ tests/unit/modules/auth/store/auth-module.spec.js
@@ -2,7 +2,7 @@ import createVuexStore from '../../../mock-data/mock-store'
 
 describe('Vuex: Pruebas en el auth-module', () => {
 
-  it.only('Estado inicial', () => {
+  it('Estado inicial', () => {
     const store = createVuexStore({
       status: 'authenticating', // 'authenticated', 'not-authenticated', 'authenticating'
       user: null,
@@ -16,6 +16,33 @@ describe('Vuex: Pruebas en el auth-module', () => {
     expect(user).toBe(null)
     expect(idToken).toBe(null)
     expect(refreshToken).toBe(null)
+  })
+
+  // Mutations
+  it('Mutation: loginUser ', () => {
+
+    const store = createVuexStore({
+      status: 'authenticating', // 'authenticated', 'not-authenticated', 'authenticating'
+      user: null,
+      idToken: null,
+      refreshToken: null,
+    })
+
+    const payload = {
+      user: { name: 'Jorge', email: 'jorge@test.com' },
+      idToken: 'abc-123',
+      refreshToken: 'xyz-123',
+    }
+
+    store.commit('auth/loginUser', payload)
+
+    const { status, user, idToken, refreshToken } = store.state.auth
+
+    expect(status).toBe('authenticated')
+    expect(user).toEqual({ name: 'Jorge', email: 'jorge@test.com' })
+    expect(idToken).toBe('abc-123')
+    expect(refreshToken).toBe('xyz-123')
+
   })
 
 })
