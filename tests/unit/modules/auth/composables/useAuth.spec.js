dfb4be03c938058908cc4b537a0450187259dfbf
Jorge Gatica <jorge.gatica@gmail.com>
2024-01-06 04:25:25 -0300

    Pruebas en composable functions: Creo constante mockStore

--- tests/unit/modules/auth/composables/useAuth.spec.js
+++ tests/unit/modules/auth/composables/useAuth.spec.js
@@ -1,5 +1,9 @@
 import useAuth from '@/modules/auth/composables/useAuth'
 
+const mockStore = {
+  dispatch: jest.fn(),
+}
+
 describe('Pruebas en useAuth', () => {
 
   it('createUser exitoso', () => {
