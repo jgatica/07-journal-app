e094643d43154556ab1a752d8586037671bcc597
Jorge Gatica <jorge.gatica@gmail.com>
2024-01-07 07:37:27 -0300

    Pruebas en el Navbar: Se crea store de usuario

--- tests/unit/modules/daybook/components/navbar.spec.js
+++ tests/unit/modules/daybook/components/navbar.spec.js
@@ -1,7 +1,18 @@
 import { shallowMount } from '@vue/test-utils'
 import Navbar from '@/modules/daybook/components/Navbar.vue'
+import createVuexStore from '../../../mock-data/mock-store'
 
 describe('Pruebas en el Navbar component', () => {
+
+  const store = createVuexStore({
+    user: {
+      name: 'Juan Carlos',
+      email: 'juan@gmail.com',
+    },
+    status: 'authenticated',
+    idToken: 'ABC',
+    refreshToken: 'XYZ',
+  })
 
   it('debe mostrar el componente correctamente', () => {
     const wrapper = shallowMount(Navbar)
