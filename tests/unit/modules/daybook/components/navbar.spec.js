6e69e25566e3588fa30e20dcb0431cdad3739262
Jorge Gatica <jorge.gatica@gmail.com>
2024-01-07 09:59:57 -0300

    Pruebas con el Router en composition API: Test al redireccionamiento y logout

--- tests/unit/modules/daybook/components/navbar.spec.js
+++ tests/unit/modules/daybook/components/navbar.spec.js
@@ -2,6 +2,23 @@ import { shallowMount } from '@vue/test-utils'
 import Navbar from '@/modules/daybook/components/Navbar.vue'
 import createVuexStore from '../../../mock-data/mock-store'
 
+import {
+  VueRouterMock,
+  createRouterMock,
+  injectRouterMock,
+} from 'vue-router-mock'
+import { config } from '@vue/test-utils'
+
+// create one router per test file
+const router = createRouterMock()
+beforeEach(() => {
+  router.reset() // reset the router state
+  injectRouterMock(router)
+})
+
+// Add properties to the wrapper
+config.plugins.VueWrapper.install(VueRouterMock)
+
 describe('Pruebas en el Navbar component', () => {
 
   const store = createVuexStore({
@@ -26,12 +43,24 @@ describe('Pruebas en el Navbar component', () => {
 
   })
 
-  it.only('click en logout, debe cerrar sesión y redireccionar ', () => {
+  it('click en logout, debe cerrar sesión y redireccionar ', async () => {
 
     const wrapper = shallowMount(Navbar, {
       global: {
         plugins: [store],
       }
+    })
+
+    await wrapper.find('button').trigger('click')
+
+    // Evaluar el router
+    expect(wrapper.router.push).toHaveBeenCalledWith({ 'name': 'login' })
+
+    expect(store.state.auth).toEqual({
+      user: null,
+      status: 'not-authenticated',
+      idToken: null,
+      refreshToken: null,
     })
 
   })
