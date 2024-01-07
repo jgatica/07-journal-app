8dbb83d19a3264eda2b37351725cdcb85900004f
Jorge Gatica <jorge.gatica@gmail.com>
2024-01-07 07:23:12 -0300

    Pruebas en el Navbar: Codigo base para test 'debe mostrar el componente correctamente'

---
+++ tests/unit/modules/daybook/components/navbar.spec.js
@@ -0,0 +1,10 @@
+import { shallowMount } from '@vue/test-utils'
+import Navbar from '@/modules/daybook/components/Navbar.vue'
+
+describe('Pruebas en el Navbar component', () => {
+
+  it('debe mostrar el componente correctamente', () => {
+    const wrapper = shallowMount(Navbar)
+  })
+
+})
