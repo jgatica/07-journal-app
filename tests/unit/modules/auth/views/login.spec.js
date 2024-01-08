26cc4c528ef20db4eb7a6c17a6cfce2719f61b7d
Jorge Gatica <jorge.gatica@gmail.com>
2024-01-07 22:32:14 -0300

    Pruebas en el login component: Solucion a error con VueRouter - Nota [[injection "Symbol(router)" not found.]]

--- tests/unit/modules/auth/views/login.spec.js
+++ tests/unit/modules/auth/views/login.spec.js
@@ -1,11 +1,40 @@
 import { shallowMount } from '@vue/test-utils'
 import Login from '@/modules/auth/views/Login.vue'
+import createVuexStore from '../../../mock-data/mock-store'
+
+import {
+  VueRouterMock,
+  createRouterMock,
+  injectRouterMock,
+} from 'vue-router-mock'
+import { config } from '@vue/test-utils'
+
+// create one router per test file
+const router = createRouterMock()
+beforeEach(() => {
+  router.reset() // reset the router state
+  injectRouterMock(router)
+})
+
+// Add properties to the wrapper
+config.plugins.VueWrapper.install(VueRouterMock)
 
 describe('Pruebas en el Login Component', () => {
 
+  const store = createVuexStore({
+    status: 'not-authenticated', // 'authenticated', 'not-authenticated', 'authenticating'
+    user: null,
+    idToken: null,
+    refreshToken: null,
+  })
+
   it('debe hacer match con el SnapShot', () => {
 
-    const wrapper = shallowMount(Login)
+    const wrapper = shallowMount(Login, {
+      global: {
+        plugins: [store],
+      }
+    })
 
     expect(wrapper.html()).toMatchSnapshot()
 
